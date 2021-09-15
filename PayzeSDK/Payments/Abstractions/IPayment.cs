fc445d58e159f80df7cad403ef42b2f2ede2b126
sasha <alexandermerabov@gmail.com>
2021-09-15 04:30:31 +0400

    Added support for Dotnet standard 2.0 Added nuget description for release of 2.0.0 version

--- PayzeSDK/Payments/Abstractions/IPayment.cs
+++ PayzeSDK/Payments/Abstractions/IPayment.cs
@@ -7,24 +7,24 @@ namespace PayzeSDK.Payments.Abstractions
     public interface IPayment
     {
         /// <exception cref="PaymentException">Throws in case of unsuccessful request</exception>
-        public JustPayResponse JustPay(JustPayRequest justPayRequest );
+        JustPayResponse JustPay(JustPayRequest justPayRequest );
 
         /// <exception cref="PaymentException">Throws in case of unsuccessful request</exception>
-        public AddCardPaymentResponse AddCard(AddCardRequest addCardRequest );
+        AddCardPaymentResponse AddCard(AddCardRequest addCardRequest );
         
         /// <exception cref="PaymentException">Throws in case of unsuccessful request</exception>
-        public PayWithCardResponse PayWithCard(PayWithCardRequest payWithCardRequest );
+        PayWithCardResponse PayWithCard(PayWithCardRequest payWithCardRequest );
         
         /// <exception cref="PaymentException">Throws in case of unsuccessful request</exception>
-        public GetTransactionInformationResponse GetTransactionInformation(GetTransactionInformationRequest getTransactionInformationRequest );
+        GetTransactionInformationResponse GetTransactionInformation(GetTransactionInformationRequest getTransactionInformationRequest );
         
         /// <exception cref="PaymentException">Throws in case of unsuccessful request</exception>
-        public RefundTransactionResponse RefundTransaction(RefundTransactionRequest refundTransactionRequest );
+        RefundTransactionResponse RefundTransaction(RefundTransactionRequest refundTransactionRequest );
         
         /// <exception cref="PaymentException">Throws in case of unsuccessful request</exception>
-        public GetMerchantBalancePaymentResponse GetMerchantBalance(GetMerchantBalanceRequest getMerchantBalanceRequest );
+        GetMerchantBalancePaymentResponse GetMerchantBalance(GetMerchantBalanceRequest getMerchantBalanceRequest );
         
         /// <exception cref="PaymentException">Throws in case of unsuccessful request</exception>
-        public CommitTransactionPaymentResponse CommitTransaction(CommitTransactionRequest commitTransactionRequest );
+        CommitTransactionPaymentResponse CommitTransaction(CommitTransactionRequest commitTransactionRequest );
     }
 }
